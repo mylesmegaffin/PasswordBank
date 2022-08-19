57cddcb1883cdb10a7d7ba49113114a18a4fc16b
mylesmegaffin <mylesmegaffin@gmail.com>
2022-08-18 22:14:31 -0400

    Updating Form Design

--- PasswordBank/Form1.Designer.cs
+++ PasswordBank/Form1.Designer.cs
@@ -29,13 +29,21 @@ namespace PasswordBank
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordBank));
             this.btn_Generate = new System.Windows.Forms.Button();
             this.tb_Password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_Generate
             // 
-            this.btn_Generate.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Generate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(81)))), ((int)(((byte)(103)))));
+            this.btn_Generate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Generate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Generate.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_Generate.FlatAppearance.BorderSize = 0;
+            this.btn_Generate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Generate.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Generate.ForeColor = System.Drawing.Color.White;
             this.btn_Generate.Location = new System.Drawing.Point(72, 185);
             this.btn_Generate.Name = "btn_Generate";
             this.btn_Generate.Size = new System.Drawing.Size(149, 39);
@@ -46,7 +54,9 @@ namespace PasswordBank
             // 
             // tb_Password
             // 
+            this.tb_Password.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tb_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.ForeColor = System.Drawing.SystemColors.WindowText;
             this.tb_Password.Location = new System.Drawing.Point(12, 57);
             this.tb_Password.Name = "tb_Password";
             this.tb_Password.ReadOnly = true;
@@ -62,8 +72,11 @@ namespace PasswordBank
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.tb_Password);
             this.Controls.Add(this.btn_Generate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PasswordBank";
-            this.Text = "Password Bank";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Password Bank - Myles Megaffin";
             this.ResumeLayout(false);
             this.PerformLayout();
 
